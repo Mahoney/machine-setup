a4eba1dc3212258f82da1ebf3ef0fb710d0fa650
Robert Elliot <a-relliot@expedia.com>
2020-01-14 10:34:19 +0000

    Add logging in to docker to m2-apitoken-gen

--- roles/expedia/scripts/m2-apitoken-gen.sh
+++ roles/expedia/scripts/m2-apitoken-gen.sh
@@ -4,9 +4,11 @@ IFS=$'\n\t'
 
 username=${1:-$USER}
 echo "Fetching a new API Token for user $username"
-API_TOKEN=$(curl -u $username --silent --show-error --fail "https://artylab.expedia.biz/api/security/encryptedPassword")
+API_TOKEN=$(curl -u "$username" --silent --show-error --fail "https://artylab.expedia.biz/api/security/encryptedPassword")
 echo "New API Token is: $API_TOKEN."
 EXPRESSION="s/<password>.*<\/password>/<password>$API_TOKEN<\/password>/g"
 sed -i -- $EXPRESSION ~/.m2/settings.xml 
 echo "maven settings are now updated!"
+echo "$API_TOKEN" | docker login kumorelease-docker-virtual.artylab.expedia.biz --username "$username" --password-stdin
+echo "logged in to docker!"
 echo "Bye!"
